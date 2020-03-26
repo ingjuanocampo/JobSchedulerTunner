097d0f92a761435044c24b2e91bbadd0560f68d9
Juan Manuel Ocampo Valencia <ing.juanocampo@gmail.com>
2020-03-26 09:13:26 -0500

    Dark time forced

--- app/src/main/java/com/ingjuanocampo/jstunner/MainActivity.kt
+++ app/src/main/java/com/ingjuanocampo/jstunner/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ingjuanocampo.jstunner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -13,6 +14,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        AppCompatDelegate.setDefaultNightMode(
+            AppCompatDelegate.MODE_NIGHT_YES);
 
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         val navController : NavController = findNavController(R.id.nav_host_fragment)
